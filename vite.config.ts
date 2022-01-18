6fcddd8c8d4327816c2fc2195f483dec8aa1aec2
yesworld <a.yesworld@gmail.com>
2022-01-18 11:17:57 +0300

    Install WindiCss

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import { presetAttributify, presetIcons, presetUno } from 'unocss'
-import Unocss from 'unocss/vite'
+import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig({
   resolve: {
@@ -32,20 +31,8 @@ export default defineConfig({
       dts: true,
     }),
 
-    // https://github.com/antfu/unocss
-    Unocss({
-      shortcuts: [
-        ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-        ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
-      ],
-      presets: [
-        presetUno(),
-        presetAttributify(),
-        presetIcons({
-          scale: 1.2,
-        }),
-      ],
-    }),
+    // https://github.com/antfu/vite-plugin-windicss
+    WindiCSS(),
   ],
 
   optimizeDeps: {
---
+++ windi.config.ts
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      width: {
+        half: '50%',
+      },
+    },
+  },
+  shortcuts: {
+    // btn: 'py-4 px-4 font-semibold rounded-lg shadow-md',
+    // 'btn-green': 'text-blue bg-green-500 hover:bg-blue-700 !important',
+    // btn: {
+    //   color: 'white',
+    //   '@apply': 'py-2 px-4 font-semibold rounded-lg',
+    //   '&:hover': {
+    //     '@apply': 'bg-red-700',
+    //     color: 'black',
+    //   },
+    // },
+    // 'btn-green': 'text-white bg-green-500 hover:bg-red-700',
+  },
+  variants: {},
+  plugins: [],
+  attributify: true,
+}
