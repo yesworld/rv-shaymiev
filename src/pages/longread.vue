1c9ddefbc48e131e41c524567860780eff52d0a6
yesworld <a.yesworld@gmail.com>
2022-01-19 14:25:10 +0300

    Add comments and edit style for blocks

--- src/pages/longread.vue
+++ src/pages/longread.vue
@@ -8,6 +8,9 @@ CommentSection.pt-10(:dude="comments[0]")
 hr.w-50.inline-flex
 CommentSection(:dude="comments[1]")
 Block02Section
+CommentSection(:dude="comments[2]")
+hr.w-50.inline-flex
+CommentSection(:dude="comments[3]")
 
 div.h-100.bg-red-100
 </template>
