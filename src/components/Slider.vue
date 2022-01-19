ed66ee1c2e85eb1b85ca16edb3b64bef9f126a7b
yesworld <a.yesworld@gmail.com>
2022-01-19 12:14:16 +0300

    Add slide bio shaymiev

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -12,14 +12,20 @@ const props = defineProps({
     }),
   },
   slides: {
+    type: Array,
+    required: true,
   },
 })
 </script>
 
 <template>
   <carousel :items-to-show="1" :settings='settings'>
-    <slide v-for="slide in 10" :key="slide">
-      <div class="carousel__item">{{ slide }}</div>
+    <slide v-for="slide in slides" :key="slide.src">
+      <div class="carousel__item">
+<!--        <div :style="`background: url('` + slide.src + `')`"></div>-->
+<!--        <div :style="{background: `url(${slide.src})`}"></div>-->
+        <img :src="slide.src"/>
+      </div>
     </slide>
 
     <template #addons>
@@ -31,15 +37,18 @@ const props = defineProps({
 
 <style lang="scss">
 .carousel__item {
-  min-height: 200px;
-  width: 100%;
-  background-color: var(--vc-clr-primary);
-  color:  var(--vc-clr-white);
-  font-size: 20px;
+  height: 600px;
+  //max-height: 800px;
+
   border-radius: 8px;
+
   display: flex;
   justify-content: center;
   align-items: center;
+  > img {
+    max-height:100%;
+    max-width: 100%;
+  }
 }
 
 .carousel__slide {
@@ -48,7 +57,25 @@ const props = defineProps({
 
 .carousel__prev,
 .carousel__next {
+  @apply bg-blue
   box-sizing: content-box;
   border: 5px solid white;
+  font-size: calc(var(--vc-nav-width) * 20);
+  border-radius: calc(var(--vc-nav-width) * 1.8);
+  width: calc(var(--vc-nav-width) * 1.8);
+  height: calc(var(--vc-nav-width) * 1.8);
+}
+.carousel__pagination-button {
+  //margin: calc(var(--vc-pgn-margin) * 1.3);
+  //width: calc(var(--vc-pgn-width) * 1.3);
+  //height: calc(var(--vc-pgn-height) * 1.3);
+  //border-radius: var(--vc-pgn-height);
+  //border: 0;
+  //cursor: pointer;
+  //background-color: var(--vc-pgn-background-color);
+  //
+  //&.carousel__pagination-button--active {
+  //  background-color: var(--vc-pgn-active-color);
+  //}
 }
 </style>
