92ee2a5a063d10e934063823e67046bec42fa0ba
yesworld <a.yesworld@gmail.com>
2022-01-19 13:13:21 +0300

    Change local setting

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 // @ts-ignore
 import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
-import { defineProps } from 'vue'
 
 // https://ismail9k.github.io/vue3-carousel/examples.html
 const props = defineProps({
