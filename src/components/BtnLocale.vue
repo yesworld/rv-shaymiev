cf9672003c47f3ad5c42e8c6a778e706eaed9b34
yesworld <a.yesworld@gmail.com>
2022-01-21 18:22:30 +0300

    Add btn locale

---
+++ src/components/BtnLocale.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const { t, availableLocales, locale } = useI18n()
+
+const toggleLocales = () => {
+  // change to some real logic
+  const locales = availableLocales
+  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
+}
+</script>
+
+<template lang="pug">
+button.btn(:title="t('button.toggle-langs')" @click="toggleLocales")
+  carbon-language
+  | {{t('button.short-lang')}}
+</template>
+
+<style lang='scss'>
+button.btn {
+  position: fixed;
+  bottom: 20px;
+  right: 25px;
+  padding-right: 25px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  z-index: 20000;
+
+  > svg {
+    font-size: 25px;
+    margin-right: 15px;
+  }
+}
+</style>
